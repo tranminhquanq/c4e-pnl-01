029197dcd5167ae4c1b0850213dab2ea566caa3a
tranminhquang <quang.tm@teko.vn>
2022-04-25 19:22:18 +0700

    lesson 04

---
+++ lesson-04/js/index.js
@@ -0,0 +1,140 @@
+// String (Chữ, chuỗi, ký tự)
+// Number (Số)
+// Boolean (Đúng/Sai) true false
+// Undefined (Không xác định)
+// Array (Mảng)
+// Object
+
+/*
+    console.log("hello world");
+    console.log(1);
+    console.log("2");
+    console.log(true);
+    console.log(false);
+    // ktra kieu du lieu
+    console.log(typeof "hello world");
+    console.log(typeof 1);
+    console.log(typeof "2");
+    console.log(typeof true);
+    */
+
+// variable
+// let VAT = 20;
+// let name;
+// console.log(name)
+// name = "Tran Minh QUang";
+// console.log(name)
+// name = "Nguyen van a";
+// console.log(name)
+// let fullName = "Tran Minh Quang";
+
+// let age = 30;
+// console.log(age);
+
+// let a = b = c = 10;
+// // let a = 10, c = 20, d;
+
+// c = 10, d = 20;
+// console.log(a);
+// console.log(c);
+// console.log(d);
+
+/*
+      Tên biến chỉ được bắt đầu bằng chữ hoặc ký tự _ $
+    */
+// console.log(VAT)
+
+// console.log(VAT);
+// console.log(VAT);
+// console.log(VAT);
+// console.log(VAT);
+
+// // Hằng
+// const PI = 3.14;
+// console.log(PI);
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(4 % 2);
+
+// // Bai 1
+// let minutes_1 = 5;
+// let seconds_1 = minutes_1 * 60;
+
+// // Bai 2
+// let temp = 57.29577951308232
+// let radians_1 = 1;
+// let degrees_1 = radians_1 * temp;
+// let radians_2 = 20;
+// let degrees_2 = radians_2 * temp;
+
+// // Bai 3
+// let celsius_1 = 10;
+// let fahrenheit_1 = celsius_1 * 9 / 5 + 32;
+// let celsius_2 = 20;
+// let fahrenheit_2 = celsius_2 * 9 / 5 + 32;
+
+// So sanh
+/*
+      >
+      <
+      >=
+      <=
+      ==
+      ===
+    */
+
+// console.log(3 > 4);
+// console.log(3 >= 4);
+// console.log(3 < 4);
+// console.log(3 <= 4);
+// console.log(3 == 4);
+// console.log(3 === "3");
+
+// let bieuThucA = 3 > 4;
+// let bieuThucB = 3 < 4;
+// // console.log(bieuThucA || bieuThucB);
+// console.log(false || true); // true
+// console.log(true || false); // true
+// console.log(true || true); // true
+// console.log(false || false); // true
+
+// console.log(false && true); // false
+// console.log(true && false); // false
+// console.log(true && true); // true
+// console.log(false && false); // false
+
+// let a = (10 + 20) > (10 - 10);
+// let b = (10 + 20) > (10 - 10) || (10 + 20) < (10 - 10);
+// let c = ((10 + 20) > (10 - 10) || (10 + 20) >= (10 - 10)) && ((10 + 20) > (10 - 10) && (10 + 20) >= (10 - 10));
+
+//
+
+// let a = 10;
+// a = a + 10;
+// a += 1;
+// a++;
+// a++;
+// a--;
+// a--;
+// console.log(a ** 3);
+
+// let firstName = "Tran";
+// let lastName = `Minh Quang`;
+// let age = 22;
+// let greeting = `Hello my name is ${firstName} ${lastName} and I'm ${age} years old`;
+
+// console.log(greeting);
+
+let sum = 2 + "2";
+
+let fullName = "Tran Minh Quang";
+
+console.log(fullName);
+fullName = 10;
+console.log(fullName);
