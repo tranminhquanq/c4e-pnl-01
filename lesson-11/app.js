b849b229370bd0e65d113b48576fa60e943d8a8e
tranminhquang <quang.tm@teko.vn>
2022-05-26 21:15:29 +0700

    ls 11

---
+++ lesson-11/app.js
@@ -0,0 +1,72 @@
+// console.log("start");
+// let a = 1;
+// let b = 2;
+// setTimeout(function () {
+//   let greeting = prompt("What is your name");
+//   console.log(greeting);
+// }, 1000);
+
+// console.log(a + b);
+
+// console.log("An toi");
+
+// setTimeout(function () {
+//   console.log("Hoc bai xong");
+// }, 0);
+
+// console.log("Di ngu");
+
+// diHocBai(diNgu);
+
+// function diHocBai(callback) {
+//   setTimeout(function () {
+//     console.log("Hoc bai xong");
+//     callback();
+//   }, 3000);
+// }
+
+// function diNgu() {
+//   console.log("Di ngu");
+// }
+
+// Promise
+
+// let myPromise = new Promise(function (resolve, reject) {
+//   let flag = true;
+//   if (flag == true) {
+//     resolve("Ok, tao da dau tu tk cua may x20");
+//   } else {
+//     reject("May bi scam");
+//   }
+// });
+
+// myPromise
+//   .then(function (kq) {
+//     let newKq = kq + " ahuuuuuu";
+//     return newKq;
+//   })
+//   .then(function (kq2) {
+//     console.log(kq2);
+//   })
+//   .catch(function (err) {
+//     console.log(err);
+//   });
+
+const movieAPI =
+  "https://api.themoviedb.org/3/trending/all/day?api_key=0a6d26d952bdd58d29ef7b7cb82a59db&language=vi";
+
+function getMovies() {
+  const request = fetch(movieAPI);
+  request
+    .then(function (req) {
+      return req.json();
+    })
+    .then(function (result) {
+      console.log(result);
+    })
+    .catch(function (err) {
+      alert(err.message);
+    });
+}
+
+getMovies();
